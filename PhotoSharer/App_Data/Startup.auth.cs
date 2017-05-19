fb02d7af9b3be743688a076ffd5c41693d220a4c
Dima Kulchitskyi <prodigy@xaker.ru>
2017-05-19 17:15:43 +0300

    identity configure

---
+++ PhotoSharer/App_Data/Startup.auth.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using PhotoSharer.Models;
+using System.Configuration;
+
+namespace PhotoSharer
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            app.UseFacebookAuthentication(
+                appId: ConfigurationManager.AppSettings["FaceBookAppId"], 
+                appSecret: ConfigurationManager.AppSettings["FaceBookSecurituCode"]);
+        }
+
+    }
+
+}
